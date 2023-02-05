6355025d7206318f3bc0503b2892ee79fc540ebf
RocketFan <kubal123.tomczak@gmail.com>
2023-02-05 01:56:13 +0100

    Move install dependecies to nvim

---
+++ install_linux.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Install dependencies
+sudo apt install -y npm
+
+# Install lst Node
+sudo npm install -g n
+sudo n latest
+
+
+#############
+# Setup vim #
+#############
+
+# Install NVIM
+sudo snap install nvim --classic
+echo 'alias vim="nvim"' >> ~/.bashrc
+
+# Install vim plugins
+nvim +PlugInstall +qall
+
+# Install formatters and linters 
+sudo npm install -g prettier
+
+# Install YCM for vim
+sudo apt install build-essential cmake python3-dev -y
+python3 ~/.local/share/nvim/plugged/youcompleteme/install.py --clangd-completer
+
+
+#################
+# Install tools #
+#################
+
+# Install LazyGit
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit /usr/local/bin
+
